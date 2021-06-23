da0f3507bb5411308c06f0b8a986f8a3f1e46d24
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2021-06-23 18:15:15 +0530

    Splits string after each delimiter

---
+++ string_split.cpp
@@ -0,0 +1,17 @@
+	vector<string> split(string & s, char delim)
+	{
+
+		vector<string> elems;
+
+		stringstream ss(s);
+		string item;
+
+		while (getline(ss, item, delim))
+		{
+			if (item.length() > 0)
+			{
+				elems.push_back(item);
+			}
+		}
+		return elems;
+	}
